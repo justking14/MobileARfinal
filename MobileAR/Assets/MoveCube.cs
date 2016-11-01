6cff6c9a68e7d2552596a71109dd6318248c76e8
justking14 <buergj@rpi.edu>
2016-11-01 17:02:36 -0400

    Unity File with Slider
    se ert fa numen cae rad

---
+++ MobileAR/Assets/MoveCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+    public float yValue = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0, yValue, 0);
+	}
+    
+    public void AdjustY(float newY){
+        yValue = newY * 5;
+    }
+}

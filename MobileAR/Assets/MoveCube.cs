6b5b2ded6d0df3504b063b142fc510f72783f8cf
justking14 <buergj@rpi.edu>
2016-11-05 17:31:38 -0400

    More buildings

--- MobileAR/Assets/MoveCube.cs
+++ MobileAR/Assets/MoveCube.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class MoveCube : MonoBehaviour {
     public float yValue = 0f;
-	// Use this for initialization
-	void Start () {
+    public float cutoffReverse = 0f;
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(0, yValue, 0);
+        transform.position = new Vector3(transform.position.x, yValue, transform.position.z);
 	}
-    
-    public void AdjustY(float newY){
-        yValue = newY * 5;
+
+    public void AdjustY(float newY) {
+        if (newY >= cutoffReverse)
+        {
+            yValue = (cutoffReverse * 5) + newY * -5;
+        }
+        else {
+            yValue = newY * 5;
+        }
     }
 }

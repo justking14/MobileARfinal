5c7e10dd69c85c7b62d2e06e3278b0faf06f1ab0
justking14 <buergj@rpi.edu>
2016-11-27 19:30:23 -0500

    prototype 1a
    stuff and other stuff

--- MobileAR/Assets/MoveCube.cs
+++ MobileAR/Assets/MoveCube.cs
@@ -2,25 +2,70 @@
 using System.Collections;
 
 public class MoveCube : MonoBehaviour {
-    public float yValue = 0f;
-    public float cutoffReverse = 0f;
+    float yValue = 0f;
+
+    public float riseTime = 0f;
+    public float fallTime = 0f;
+
+    public bool incrementally = true;
+
+    float startY = 0f;
+
     // Use this for initialization
     void Start () {
-	
+        
+
+        startY = transform.position.y;
+        AdjustY(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(transform.position.x, yValue, transform.position.z);
+        transform.position = new Vector3(transform.position.x, startY + yValue, transform.position.z);
 	}
 
     public void AdjustY(float newY) {
-        if (newY >= cutoffReverse)
+        Debug.Log(newY);
+
+        if (incrementally == false)
         {
-            yValue = (cutoffReverse * 5) + newY * -5;
+            if (newY > fallTime)
+            {
+                yValue = -startY * 5.0f;
+            }
+            else if (newY > riseTime)
+            {
+                yValue = 0;
+            }
+            else
+            {
+                yValue = -startY * 5.0f;
+            }
         }
         else {
-            yValue = newY * 5;
+            if (newY > fallTime)
+            {
+                yValue = -startY * 5.0f;
+            }
+            else if (newY <= riseTime - 0.05f && newY > riseTime - 0.1f)
+            {
+                yValue = -startY;
+            }
+            else if (newY <= riseTime && newY > riseTime - 0.05f)
+            {
+                yValue = -startY / 2.0f;
+
+            }
+            else if (newY > riseTime)
+            {
+                yValue = 0;
+            }
+            else
+            {
+                yValue = -startY * 5.0f;
+            }
+
         }
+
     }
 }

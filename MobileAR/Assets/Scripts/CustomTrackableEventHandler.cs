fedefad4d0c43c29c2bed033651b4a0735ddbfbc
Dan Gorman <danthemangorman@gmail.com>
2016-11-29 03:25:30 -0500

    enabled current map prototype to work with vuforia stones target

---
+++ MobileAR/Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System;
+
+/*
+ * This is a template for a custom trackable event handler
+ * Replace the DefaultTrackableEventHandler script with a child
+ * Not to be used alone
+ */
+
+public class CustomTrackableEventHandler: MonoBehaviour, ITrackableEventHandler {
+
+    TrackableBehaviour mTrackableBehavior;
+
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+
+    protected virtual void Init() {
+        mTrackableBehavior = GetComponent<TrackableBehaviour>();
+
+        if (mTrackableBehavior) {
+            mTrackableBehavior.RegisterTrackableEventHandler(this);
+        }
+
+        OnTrackingLost();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        DoUpdate();
+	}
+
+    protected virtual void DoUpdate() {
+
+    }
+
+    void SetVisibility(bool visible) {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in rendererComponents) {
+            r.enabled = visible;
+        }
+    }
+
+    protected virtual void OnTrackingFound() {
+        SetVisibility(true);
+    }
+
+    protected virtual void OnTrackingLost() {
+        SetVisibility(false);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+        Debug.Log(newStatus.ToString());
+        switch (newStatus) {
+            case TrackableBehaviour.Status.TRACKED:
+                OnTrackingFound();
+                break;
+            case TrackableBehaviour.Status.NOT_FOUND:
+                OnTrackingLost();
+                break;
+            default:
+                break;
+
+        }
+    }
+}
---
+++ MobileAR/Assets/Scripts/MapTrackableEventHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+/*
+ * This is a template for a custom trackable event handler
+ * Replace the DefaultTrackableEventHandler script with a derivitive of this class
+ * Not to be used alone
+ */
+
+public class MapTrackableEventHandler : CustomTrackableEventHandler {
+
+    public GameObject slider;
+
+    TrackableBehaviour mTrackableBehavior;
+
+    // Use this for initialization
+    void Start() {
+        Init();
+    }
+
+    protected override void Init() {
+        base.Init();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        DoUpdate();
+    }
+
+    protected override void DoUpdate() {
+        base.DoUpdate();
+    }
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+        slider.SetActive(true);
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingLost();
+        slider.SetActive(false);
+    }
+
+}

632c1d796261def8f69ce0688e1160b328413b19
Dan Gorman <danthemangorman@gmail.com>
2016-11-28 22:48:20 -0500

    still just fiddling with things, made the map more extensible

---
+++ MobileAR/Assets/Scripts/XLookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XLookAtCamera : MonoBehaviour {
+
+    Transform t;
+
+	// Update is called once per frame
+	void Update () {
+        t = Camera.main.transform;
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.x=Mathf.Atan((t.position.y - transform.position.y) / (-1*(t.position.z - transform.position.z)))*Mathf.Rad2Deg;
+        transform.rotation=Quaternion.Euler(rot);
+	}
+}

7ee2ea779bd592f5a23e21e141274b6f03555dce
lrvolle <lrvolle@gmail.com>
2016-12-01 13:02:12 -0500

    Added autofocusing to AR Camera

---
+++ MobileAR/Assets/Scripts/ARCameraSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ARCameraSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!focusModeSet)
+        {
+            Debug.Log("Failed to set focus mode to continusauto (unsupported mode).");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

3668c2845020f31d3f5152313d939a19beb8d9f5
lrvolle <lrvolle@gmail.com>
2016-12-05 13:05:14 -0500

    Added new targets

---
+++ MobileAR/Assets/Scripts/SliderLabels.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderLabels : MonoBehaviour {
+    public GameObject decadeText;
+	// Use this for initialization
+	void Start () {
+        GameObject canvas = GameObject.Find("SliderCanvas");
+        Text fifties = Instantiate(decadeText, canvas.transform.position, transform.rotation) as Text;
+        fifties.fontSize = 15;
+        fifties.text = "50's";
+        fifties.transform.SetParent(canvas.transform, false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
